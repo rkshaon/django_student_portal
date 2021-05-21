76bc92fa3e00daf168252b8901ffe5e8bc9cb22c
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-05-22 05:26:11 +0600

    modules view and url done

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from classroom.views import categories, category_courses, new_course, enroll, delete_course, edit_course, my_courses
+from module.views import new_module, course_modules
 
 urlpatterns = [
     # course - clasroom views
@@ -11,4 +12,7 @@ urlpatterns = [
     path('<course_id>/enroll/', enroll, name='enroll'),
     path('<course_id>/edit', edit_course, name='edit_course'),
     path('<course_id>/delete', delete_course, name='delete_course'),
+    # modules - module views
+    path('<course_id>/modules', course_modules, name='course_modules'),
+    path('<course_id>/modules/new-module', new_module, name='new_module'),
 ]
--- module/views.py
+++ module/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.template import loader
 
 from module.forms import NewModuleForm
 
@@ -9,11 +11,43 @@ def new_module(request, course_id):
     user = request.user
     course = get_object_or_404(Course, id=course_id)
 
-    if request.method == 'POST':
-        form = NewModuleForm(request.POST)
-        if form.is_valid():
-            title = form.cleaned_data.get('title')
-            hours = form.cleaned_data.get('hours')
+    if user != course.user:
+        return HttpResponseForbidden()
+    else:
+        if request.method == 'POST':
+            form = NewModuleForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                hours = form.cleaned_data.get('hours')
 
-            m, created = Module.objects.get_or_create(title=title, hours=hours, user=user)
-            
+                m, created = Module.objects.get_or_create(title=title, hours=hours, user=user)
+                course.modules.add(m)
+                course.save()
+
+                # return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+                return redirect('modules', course_id=course_id)
+        else:
+            form = NewModuleForm()
+
+    context = {
+        'form': form,
+    }
+
+    template = loader.get_template('module/newmodule.html')
+    return HttpResponse(template.render(context, request))
+
+def course_modules(request, course_id):
+    user = request.user
+    course = get_object_or_404(Course, id=course_id)
+    teacher_mode = False
+
+    if user == course.user:
+        teacher_mode = True
+
+    context = {
+        'form': form,
+        'teacher_mode': teacher_mode,
+    }
+
+    template = loader.get_template('module/modules.html')
+    return HttpResponse(template.render(context, request))
