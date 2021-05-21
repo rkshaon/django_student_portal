c5c577264d8c84c720cb94cd3d2779ebf815843d
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-05-22 02:55:24 +0600

    module model

--- module/models.py
+++ module/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Module(models.Model):
+    """docstring for Module."""
+    title = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Module_Owner')
+    hours = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
