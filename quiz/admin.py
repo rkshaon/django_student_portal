bc3d3dd14ac031c0971ad60cafb4f47c2c7a00a6
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-05-24 23:43:35 +0600

    quiz admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz.models import Answer, Question, Quizzes, Attempter, Attempt
+
+admin.site.register(Answer)
+admin.site.register(Question)
+admin.site.register(Quizzes)
+admin.site.register(Attempter)
+admin.site.register(Attempt)
